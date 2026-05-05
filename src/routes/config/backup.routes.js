e2d3c53216ee193c1e2f971c8ae7ee29079138ed
Enma586 <enmaflore7421@gmail.com>
2026-05-04 19:03:36 -0600

    add trigger backup on config update

--- src/routes/config/backup.routes.js
+++ src/routes/config/backup.routes.js
@@ -1,10 +1,14 @@
 import { Router } from 'express';
-import { downloadBackup } from '../../controllers/config/Backup.controller.js';
+import { downloadBackup, triggerAutomaticBackup } from '../../controllers/config/Backup.controller.js';
 import { auth, roleGuard } from '../../middlewares/index.js';
 
 const router = Router();
 
-// Endpoint protegido
+// Endpoint para el botón de la coordinadora (Descarga directa)
 router.get('/download', auth, roleGuard('Coordinador'), downloadBackup);
+
+// Endpoint para el "Vigilante de Arranque" (Ejecución interna)
+// Este podría ser accedido solo por el servidor o un usuario privilegiado
+router.post('/trigger-auto', auth, roleGuard('Coordinador'), triggerAutomaticBackup);
 
 export default router;
